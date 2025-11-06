c60d24e4a0eb6d44655decd424e9a8e72784bbae
sarah mughal <37761665+sarahmughal@users.noreply.github.com>
2025-11-05 20:38:50 -0800

    delete

--- dna-to-protein-portfolio/tests/test_translate.py
+++
@@ -1,8 +0,0 @@
-from src.translate import dna_to_rna, translate_dna
-
-def test_dna_to_rna_basic():
-    assert dna_to_rna('ACTT') == 'ACUU'
-
-def test_translate_simple():
-    # ATG -> AUG -> M
-    assert translate_dna('ATG') == 'M'
